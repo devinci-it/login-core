2a8ef4323404e0a7d10a26561124da694d885164
devinci-it <vince.it@icloud.com>
2024-03-24 12:17:46 +0000

    Chore:unit test

---
+++ Tests/Commands/SetupLoginCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Commands;
+
+use PHPUnit\Framework\TestCase;
+use Devinci\LaravelEssentials\Commands\SetupLoginCommand;
+use Devinci\LaravelEssentials\LoginCoreServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Console\Kernel;
+
+class SetupLoginCommandTest extends TestCase
+{
+
+
+}
