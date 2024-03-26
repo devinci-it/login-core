535e91fb3df6281c6afac3fa950f0bf8862bc97d
devinci-it <vince.it@icloud.com>
2024-03-26 10:22:35 -0700

    fix

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,30 +1,30 @@
 @include('header')
 
 <!-- Main content for user login -->
-    <main class="main-content" id="login-content">
-        <div class="container">
-            <div class="form-wrapper">
-                <h2 class="title-medium-text">USER LOGIN</h2>
-                <form action="/login" method="POST">
-                    @csrf
-                    <label class="caption-text dark">
-                        <input placeholder="Username" class="input form-input" type="text" name="username" required>
-                    </label>
-                    <br>
-                    <label class="caption-text dark">
-                        <input placeholder="Password" type="password" class="input form-input" name="password" required>
+<main class="main-content" id="login-content">
+    <div class="container">
+        <div class="form-wrapper">
+            <h2 class="title-medium-text">USER LOGIN</h2>
+            <form action="/login" method="POST">
+                @csrf
+                <label class="caption-text dark">
+                    <input placeholder="Username" class="input form-input" type="text" name="username" required>
+                </label>
+                <br>
+                <label class="caption-text dark">
+                    <input placeholder="Password" type="password" class="input form-input" name="password" required>
 
-                    </label>
-                    <br>
-                    <input class="btn submit-button" name="submit" type="submit" value="Login">
-                </form>
-                    <div class="link_wrapper flex">
-                        <a class="caption-text dark" href="/register"> Register</a>
-                        <a class="caption-text dark" href="#"> Forgot Password</a>
-                    </div>
+                </label>
+                <br>
+                <input class="btn submit-button" name="submit" type="submit" value="Login">
+            </form>
+            <div class="link_wrapper flex">
+                <a class="caption-text dark" href="/register"> Register</a>
+                <a class="caption-text dark" href="#"> Forgot Password</a>
             </div>
         </div>
-    </main>
+    </div>
+</main>
 
 </body>
 </html>
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -7,22 +7,22 @@
             <h2 class="title-medium-text">REGISTER</h2>
             <hr/>
 
-    <form action="/register" method="POST">
-        @csrf
+            <form action="/register" method="POST">
+                @csrf
 
-        <input value="" class="input form-input" name="first_name" type="text" placeholder="First Name" required>
-        <input value="" class="input form-input" name="last_name" type="text" placeholder="Last Name" required>
-        <input value="" class="input form-input" name="email" type="email" placeholder="Email" required>
-        <input value="" class="input form-input" name="username" type="text" placeholder="Username" required>
-        <input value="" class="input form-input" name="password" type="password" placeholder="Password" required>
-        <input value="" class="input form-input" name="password_confirmation" type="password" placeholder="Verify Password"
-            required>
+                <input value="" class="input form-input" name="first_name" type="text" placeholder="First Name" required>
+                <input value="" class="input form-input" name="last_name" type="text" placeholder="Last Name" required>
+                <input value="" class="input form-input" name="email" type="email" placeholder="Email" required>
+                <input value="" class="input form-input" name="username" type="text" placeholder="Username" required>
+                <input value="" class="input form-input" name="password" type="password" placeholder="Password" required>
+                <input value="" class="input form-input" name="password_confirmation" type="password" placeholder="Verify Password"
+                       required>
 
-        <input type="submit" class="btn submit-button" name="signup" value="Register">
-        <p class="caption-text" style="color: #ccc; margin-bottom: 10px; padding: 5px;">
-            By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
-        </p>
-    </form>
+                <input type="submit" class="btn submit-button" name="signup" value="Register">
+                <p class="caption-text" style="color: #ccc; margin-bottom: 10px; padding: 5px;">
+                    By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
+                </p>
+            </form>
         </div>
     </div>
 </main>
